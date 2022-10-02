e9c087ad07e98513945bd7fa501ee61e498d3b92
Bhavesh-Khandar <khandarbhavesh7@gmail.com>
2022-10-02 19:58:16 +0530

    Project Last

--- card1.js
+++ card1.js
@@ -97,4 +97,14 @@ var card1data = [
 
     })
 
-  
+    let nav=document.querySelector(".navbar")
+    window.onscroll=() =>
+    {
+        if(window.scrollY > 200)
+        {
+            nav.classList.add('navcolor')
+        }
+        else{
+            nav.classList.remove('navcolor')
+        }
+    }
--- card2.js
+++ card2.js
@@ -101,4 +101,14 @@ var card1data = [
 
     })
 
-  
+    let nav=document.querySelector(".navbar")
+    window.onscroll=() =>
+    {
+        if(window.scrollY > 200)
+        {
+            nav.classList.add('navcolor')
+        }
+        else{
+            nav.classList.remove('navcolor')
+        }
+    }
